fdc382b9765bcc4ba52ab27cf46453211fcfad5d
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-06-19 08:09:31 +0200

    feat: gatsby-browser add user prompt to reload on onServiceWorkerUpdateReady

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,11 @@ export const wrapRootElement = ({ element }) => {
 export const wrapPageElement = ({ element, props }) => {
   return <PageComponents {...props}>{element}</PageComponents>
 }
+
+// https://gatsbyjs.org/docs/add-offline-support-with-a-service-worker
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This site has been updated. Reload to display the latest version?`
+  )
+  if (answer === true) window.location.reload()
+}
